a2ffa3a5c8073d7ac6c18ecab35e9269ffed117f
B-Mahl <banelemahlangu@gmail.com>
2023-03-16 05:04:02 -0700

    fixed hearder file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#iinclude "main.h"
 #include <stdio.h>
 #define UNUSED(x) (void)(x)
 /**
- * 
+ * main - main function
  * @argc: argumentc
  * @argv: vector of arguments
- *
+ *Return: always 0
  */
 int main(int argc, char *argv[])
 {
