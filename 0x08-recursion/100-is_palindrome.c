d13b70e70fe5b9257e88beb3080ed8ae30fef1f2
B-Mahl <banelemahlangu@gmail.com>
2023-09-01 07:20:24 -0700

    Fixed betty

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,54 @@
+/**
+ * get_length - gets length of string
+ * @*s: value at address
+ * @s: stores address of string
+ * Return: Length of string
+ */
+int get_length(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * is_palindrome_helper - recursivley checks characters
+ * @*s: value at address
+ * @s: pointer stores address
+ * @start: start of string
+ * @end: end of string
+ * Return: Boolean
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+/**
+ * is_palindrome - is string palindrome
+ * @s: pointer to string
+ * @*s: value at address
+ * Return: Boolean
+ */
+int is_palindrome(char *s)
+{
+	int length;
+
+	length = get_length(s);
+	if (length == 0)
+	{
+		return (1);
+	}
+	return (is_palindrome_helper(s, 0, length - 1));
+}
