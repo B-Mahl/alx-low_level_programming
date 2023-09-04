8b4851dc36542910473a91c58bd6fdbd2900b05c
B-Mahl <banelemahlangu@gmail.com>
2023-09-04 14:55:20 -0700

    Fixed comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,8 @@
  * main - program prints its name
  * @argc: argument count
  * @argv: argument vector
+ * Return: 0 for seccessful execution
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
