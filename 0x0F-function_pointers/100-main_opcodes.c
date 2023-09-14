3df4e14055caaf1d5404574404a0e9fc74b44dfe
B-Mahl <banelemahlangu@gmail.com>
2023-09-14 03:04:28 +0200

    advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */ 
+int main(int argc, char *argv[])
+{
+	int memry, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	memry = atoi(argv[1]);
+
+	if (memry < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < memry; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == memry - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
