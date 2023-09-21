fb733e4d73d9d955964ba111702c496fc215bf5f
B-Mahl <banelemahlangu@gmail.com>
2023-09-22 01:59:24 +0200

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * beforeMain - prints before the main function is executed.
+ */
+void beforeMain(void) __attribute__((constructor));
+
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
