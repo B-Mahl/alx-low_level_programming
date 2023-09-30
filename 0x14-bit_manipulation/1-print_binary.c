684122d7425936ff7c58d6c5710879e0703aaadd
B-Mahl <banelemahlangu@gmail.com>
2023-09-30 02:26:10 +0200

    syntax error fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 		return;
 	}
 	b = n;
-	for (i = 0; n != 0; n = n >> 1; i++)
+	for (i = 0; n != 0; n = n >> 1, i++)
 		;
 	for (i -= 1; i >= 0; i--)
 		_putchar(48 + ((b >> i) & 1));
