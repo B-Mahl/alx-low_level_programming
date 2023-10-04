4eb5b98f061bdfecb0c45fbb2976cc2a820ea60b
B-Mahl <banelemahlangu@gmail.com>
2023-10-04 03:02:36 +0200

    2nd attempt

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,16 +16,19 @@ void error_exit(int code, const char *message)
 
 int main(int argc, char *argv[])
 {
+	int fd_from, fd_to;
+	const char *file_from;
+	const char *file_to;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_r, bytes_wr;
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to");
 
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
+	file_from = argv[1];
+	file_to = argv[2];
 
-	int fd_from = open(file_from, O_RDONLY);
+	fd_from = open(file_from, O_RDONLY);
 
 	if (fd_from == -1)
 	{
@@ -33,7 +36,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	if (fd_to == -1)
 	{
@@ -73,6 +76,6 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	return 0;
+	return (0);
 }
 
